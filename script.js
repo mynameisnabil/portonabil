dbf44d94c10eebdcda085fa2d528ff7b781eb142
nabil <mynameisnabil09@gmail.com>
2023-09-29 15:05:41 +0700

    Update Navbar Mobile

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const smMenuLink1 = document.querySelector('.main-header__sm-menu-link--1')
 const smMenuLink2 = document.querySelector('.main-header__sm-menu-link--2')
 const smMenuLink3 = document.querySelector('.main-header__sm-menu-link--3')
 const smMenuLink4 = document.querySelector('.main-header__sm-menu-link--4')
+const smMenuLink5 = document.querySelector('.main-header__sm-menu-link--5')
+const smMenuLink6 = document.querySelector('.main-header__sm-menu-link--6')
 
 smMenuBtn.addEventListener('click', () => {
   smMenu.style.transitionDelay = '0s'
@@ -27,23 +29,42 @@ smMenuBtn.addEventListener('click', () => {
   smMenuLink4.style.transitionDelay = '1.4s'
   smMenuLink4.style.transform = 'translateY(0)'
   smMenuLink4.style.opacity = '1'
+
+  smMenuLink5.style.transitionDelay = '1.7s'
+  smMenuLink5.style.transform = 'translateY(0)'
+  smMenuLink5.style.opacity = '1'
+
+  smMenuLink6.style.transitionDelay = '2s'
+  smMenuLink6.style.transform = 'translateY(0)'
+  smMenuLink6.style.opacity = '1'
+
 })
 
 smMenuLinks.forEach((ele) => {
   ele.addEventListener('click', () => {
-    smMenuLink4.style.transitionDelay = '0s'
+
+    smMenuLink5.style.transitionDelay = '0s'
+    smMenuLink5.style.transform = 'translateY(50px)'
+    smMenuLink5.style.opacity = '0'
+
+    smMenuLink6.style.transitionDelay = '.3s'
+    smMenuLink6.style.transform = 'translateY(50px)'
+    smMenuLink6.style.opacity = '0'
+
+
+    smMenuLink4.style.transitionDelay = '6s'
     smMenuLink4.style.transform = 'translateY(50px)'
     smMenuLink4.style.opacity = '0'
 
-    smMenuLink3.style.transitionDelay = '.3s'
+    smMenuLink3.style.transitionDelay = '.9s'
     smMenuLink3.style.transform = 'translateY(50px)'
     smMenuLink3.style.opacity = '0'
 
-    smMenuLink2.style.transitionDelay = '.6s'
+    smMenuLink2.style.transitionDelay = '.12s'
     smMenuLink2.style.transform = 'translateY(50px)'
     smMenuLink2.style.opacity = '0'
 
-    smMenuLink1.style.transitionDelay = '.9s'
+    smMenuLink1.style.transitionDelay = '.15s'
     smMenuLink1.style.transform = 'translateY(50px)'
     smMenuLink1.style.opacity = '0'
 
@@ -57,19 +78,29 @@ smMenuLinks.forEach((ele) => {
 })
 
 smMenuCloseBtn.addEventListener('click', () => {
-  smMenuLink4.style.transitionDelay = '0s'
+
+
+  smMenuLink6.style.transitionDelay = '0s'
+  smMenuLink6.style.transform = 'translateY(50px)'
+  smMenuLink6.style.opacity = '0'
+
+  smMenuLink5.style.transitionDelay = '3s'
+  smMenuLink5.style.transform = 'translateY(50px)'
+  smMenuLink5.style.opacity = '0'
+
+  smMenuLink4.style.transitionDelay = '6s'
   smMenuLink4.style.transform = 'translateY(50px)'
   smMenuLink4.style.opacity = '0'
 
-  smMenuLink3.style.transitionDelay = '.3s'
+  smMenuLink3.style.transitionDelay = '.9s'
   smMenuLink3.style.transform = 'translateY(50px)'
   smMenuLink3.style.opacity = '0'
 
-  smMenuLink2.style.transitionDelay = '.6s'
+  smMenuLink2.style.transitionDelay = '.12s'
   smMenuLink2.style.transform = 'translateY(50px)'
   smMenuLink2.style.opacity = '0'
 
-  smMenuLink1.style.transitionDelay = '.9s'
+  smMenuLink1.style.transitionDelay = '.15s'
   smMenuLink1.style.transform = 'translateY(50px)'
   smMenuLink1.style.opacity = '0'
 
